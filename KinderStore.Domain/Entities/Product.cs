d0129fb32a534cad5369c28ea14fa42aadb94276
Anton Tarasiuk <anton_tarasiuk@epam.com>
2016-02-10 11:27:11 +0300

    Added some data and images.

--- KinderStore.Domain/Entities/Product.cs
+++ KinderStore.Domain/Entities/Product.cs
@@ -9,13 +9,14 @@ namespace KinderStore.Domain.Entities
 	public class Product
 	{
 		public int ProductId { get; set; }
-		//public int Code { get; set; }
 		public string Name { get; set; }
-		//public string Size { get; set; }
-		//public string Material { get; set; }
+		public string Code { get; set; }
+		public string Size { get; set; }
+		public string Material { get; set; }
 		public string Description { get; set; }
 		public string Category { get; set; }
 		public decimal Price { get; set; }
-		//public bool IsAvailable { get; set; }
+		public string Image { get; set; }
+		public bool IsAvailable { get; set; }
 	}
 }
